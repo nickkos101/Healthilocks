19cf23a7ca7d6049834f25ea374ceb59cdc8f153
Nicholas Koskowski <Nick@businessonmarketst.com>
2014-07-01 10:17:29 -0700

    Added initial files
    pre-wp hookup qc'ed with alvaro

---
+++ function.js
@@ -0,0 +1,109 @@
+$(document).ready(function(){
+
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > 1140) {
+			$('#droplet').css({'position':'fixed', 'left': 'auto', 'top': '265px', 'marginLeft': '20px'})
+		}
+
+		if ($(window).scrollTop() < 1140) {
+			$('#droplet').css({'position':'absolute', 'left': '20px', 'top': '-45px', 'marginLeft': '0px'})
+		}
+	});
+
+	var yPos = -120;
+  var scrollPos = window.pageYOffset;
+
+  function MouseWheelHandler(e) {
+    if ($(window).scrollTop() > 1870) {
+     if (scrollPos < window.pageYOffset) {
+      yPos += 40;
+      if(yPos > 0) {
+       yPos = 0;
+     }
+     $('.cross-vert').css('background-position', '0 '+yPos);
+     $('.cross-horz').css('background-position', '0 '+(yPos-40));
+   } else {
+    yPos -= 40;
+    if(yPos < -120) {
+      yPos = -120;
+    }
+    $('.cross-vert').css('background-position', '0 '+yPos);
+    $('.cross-horz').css('background-position', '0 '+(yPos-40));
+  }
+}
+scrollPos = window.pageYOffset;
+}
+
+window.onscroll = MouseWheelHandler;
+
+
+$('.slide-bank').nivoSlider({
+    effect: 'random',               // Specify sets like: 'fold,fade,sliceDown'
+    slices: 15,                     // For slice animations
+    boxCols: 8,                     // For box animations
+    boxRows: 4,                     // For box animations
+    animSpeed: 500,                 // Slide transition speed
+    pauseTime: 3000,                // How long each slide will show
+    startSlide: 0,                  // Set starting Slide (0 index)
+    directionNav: false,             // Next & Prev navigation
+    controlNav: true,               // 1,2,3... navigation
+    controlNavThumbs: false,        // Use thumbnails for Control Nav
+    pauseOnHover: true,             // Stop animation while hovering
+    manualAdvance: false,           // Force manual transitions
+    prevText: 'Prev',               // Prev directionNav text
+    nextText: 'Next',               // Next directionNav text
+    randomStart: false,             // Start on a random slide
+    beforeChange: function(){},     // Triggers before a slide transition
+    afterChange: function(){},      // Triggers after a slide transition
+    slideshowEnd: function(){},     // Triggers after all slides have been shown
+    lastSlide: function(){},        // Triggers when last slide is shown
+    afterLoad: function(){}         // Triggers when slider has loaded
+  });
+
+
+$('.product-bank').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  swipe: false,
+  draggable: false,
+  arrows: true
+});
+
+$('.vid-mod .mask').click(function(){
+  $('.lightbox').show();
+  $('.lightbox-overlay').show();
+});
+
+$('.signup').click(function(){
+  $('.signup-form').toggle();
+});
+
+$('.lightbox-overlay').click(function(){
+  $('.lightbox').hide();
+  $('.lightbox-overlay').hide();
+});
+
+$('.product').click(function(){
+  if($(this).hasClass('left')) {
+    $('.divet.left').animate({left: '158.5px'});
+  }
+  if($(this).hasClass('mid')) {
+    $('.divet.left').animate({left: '478.5px'});
+  }
+  if($(this).hasClass('right')) {
+    $('.divet.left').animate({left: '794.5px'});
+  }
+  var title = $(this).find('h3').text();
+  $('.product-display h3').text(title);
+  var img = $(this).find('.thumb').attr('src');
+  $('.ft-img img').attr('src', img);
+  var text = $(this).find('p').text();
+  $('.product-display p').text(text);
+  var price = $(this).find('.price').text();
+  $('.product-display .price').text(price);
+});
+
+
+});
+
---
+++ index.php
@@ -0,0 +1,231 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="normalize.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href='http://fonts.googleapis.com/css?family=Gudea:400,700' rel='stylesheet' type='text/css'>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="nivo.js" type="text/javascript"></script>
+	<link rel="stylesheet" href="nivo.css" type="text/css" />
+	<link rel="stylesheet" type="text/css" href="http://cdn.jsdelivr.net/jquery.slick/1.3.6/slick.css"/>
+	<script type="text/javascript" src="http://cdn.jsdelivr.net/jquery.slick/1.3.6/slick.min.js"></script>
+	<script src="function.js"></script>
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div class="logo">
+				<img src="images/logo.png">
+			</div>
+			<nav>
+				<ul>
+					<li><a href="">Shop</a></li>
+					<li><a href="">Where to Buy</a></li>
+					<li><a href="">Treatment</a></li>
+					<li><a href="">About Us</a></li>
+					<li><a href="">Faq's</a></li>
+					<li><a href="">Contact</a></li>
+				</ul>
+			</nav>
+		</div>
+		<div class="container large">
+			<div class="signup">
+				<h2>Sign<br/> Up</h2>
+			</div>
+		</div>
+		<div class="container large">
+			<div class="signup-form">
+				<div class="container">
+					<p><i>Sign up for our newsletter to stay informed about our latest products and upcoming events.</i></p>
+					<input type="text" placeholder="First Name">
+					<input type="text" placeholder="Last Name">
+					<input type="text" placeholder="eMail">
+					<button>Submit</button>
+				</div>				
+			</div>
+		</div>
+	</header>
+	<section>
+		<div class="slider">
+			<div class="container">
+				<div class="slide-bank">
+					<img src="http://jeannemaureens.com/wp-content/uploads/2014/05/beautiful-hair2.jpg">
+					<img src="http://www.yourbeauty411.com/wp-content/uploads/2010/09/Beach_wave_hair.jpg">
+					<img src="http://www.sparqvault.com/wp-content/uploads/2012/08/vanessa-hudgens-beach-hair.jpg">
+				</div>
+				<div class="content-bank">
+					<div>
+						<img src="images/health-cross.png">
+						<h2>Improve volume, managability & overall health.</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="product-carousel">
+				<div class="product-bank">
+					<div class="product left">
+						<img src="images/product.png">
+						<img class="thumb" src="images/product-large.png">
+						<h3>Daily Shampoo</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris.</p>
+							<span class="price">$42.00</span>
+						</div>
+						<div class="product mid">
+							<img src="images/product2.png">
+							<img class="thumb" src="images/product-large.png">
+							<h3>Daily Conditioner</h3>
+							<img class="divet" src="images/divet.png">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+								quis nostrud exercitation ullamco laboris.</p>
+								<span class="price">$42.00</span>
+							</div>
+							<div class="product right">
+								<img src="images/product3.png">
+								<img class="thumb" src="images/product-large.png">
+								<h3>Serum Salon - Treatment </h3>
+								<img class="divet" src="images/divet.png">
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+									quis nostrud exercitation ullamco laboris.</p>
+									<span class="price">$42.00</span>
+								</div>
+								<div class="product left">
+									<img src="images/product.png">
+									<img class="thumb" src="images/product-large.png">
+									<h3>Daily Salon - Treatment </h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+										quis nostrud exercitation ullamco laboris.</p>
+										<span class="price">$42.00</span>
+									</div>
+									<div class="product mid">
+										<img src="images/product.png">
+										<img class="thumb" src="images/product-large.png">
+										<h3>Daily Salon - Treatment </h3>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+											tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+											quis nostrud exercitation ullamco laboris.</p>
+											<span class="price">$42.00</span>
+										</div>
+										<div class="product right">
+											<img src="images/product.png">
+											<img class="thumb" src="images/product-large.png">
+											<h3>Daily Salon - Treatment </h3>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+												tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+												quis nostrud exercitation ullamco laboris.</p>
+												<span class="price">$42.00</span>
+											</div>
+										</div>
+									</div>
+									<div class="divet-bank">
+										<img class="divet left" src="images/divet.png">
+									</div>
+								</div>
+								<div class="product-display">
+									<div class="container">
+										<div class="ft-img">
+											<img src="images/product-large.png">
+										</div>
+										<h3>Healthilocks Daily Shampoo</h3>
+										<p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. </p>
+										<div class="buttons">
+											<button>Where to Buy</button>
+											<button>Order Online</button>
+										</div>
+										<span class="price">$38.00</span>
+									</div>
+								</div>
+								<div class="container large">
+									<div class="wrap">
+										<img src="images/section-image.png">
+										<div class="container">
+											<div class="text-sect">
+												<img id="droplet" src="images/droplet.png">
+												<h2>with Plexprotien &trade;</h2>
+											</div>
+											<div class="vid-mod">
+												<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+													tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+													quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+													consequat.</p>
+													<span class="cross-plate"> 
+														<img class="mask" src="http://blogs.windows.com/cfs-file.ashx/__key/CommunityServer-Blogs-Components-WeblogFiles/00-00-00-53-84-metablogapi/2133.image_5F00_4582ED60.png">
+														<img src="images/small-cross.png" class="x-left">
+														<img src="images/small-cross.png" class="x-right">
+														<div class="lightbox">
+															<iframe width="100%" height="400" src="//www.youtube.com/embed/6YaCZsgh5yE" frameborder="0" allowfullscreen></iframe>
+														</div>
+														<div class="lightbox-overlay">
+														</div>
+													</span>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="locator">
+										<div class="container">
+											<div class="left">
+												<div class="cross">
+													<div class="cross-vert colored">
+													</div>
+													<div class="cross-horz colored">
+													</div>
+												</div>
+												<div class="wrap">
+													<h4>Why Healthilocks?</h4>
+													<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+														tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+														quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+														consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+														cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+														proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+													</div>
+												</div>
+												<div class="right">
+													<h4>Salon Locator</h4>
+													<div style="border:5px solid yellow; overflow:hidden; -moz-box-sizing: border-box; -webkit-box-sizing: border-box; box-sizing: border-box;">
+														<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d26821.24545980593!2d-117.1461504!3d32.82788695!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1403814283933" width="390" height="230" frameborder="0" style="border:0"></iframe>
+													</div>
+												</div>
+											</div>
+										</div>
+									</section>
+									<footer>
+										<div class="container">
+											<nav>
+												<ul>
+													<li><a href="">Shop</a></li>
+													<li><a href="">Where to buy</a></li>
+													<li><a href="">Treatment</a></li>
+													<li><a href="">About Us</a></li>
+													<li><a href="">FAQs</a></li>
+													<li><a href="">Contact</a></li>
+												</ul>
+											</nav>
+											<p>
+												800.431.1365<br/>
+												info@healthilocks.com<br/>
+												<br/>
+												<b>Mailing Address:</b><br/>
+												Tru-Hair LLC.<br/>
+												7040 Avenida Encinias, Ste. 104-40<br/>
+												Carlsbad, CA 92011
+											</p>
+											<div class="social-media">
+												<img src="images/facebook.png">
+												<img src="images/pinterest.png">
+												<img src="images/twitter.png">
+												<img src="images/youtube.png">
+											</div>
+											<div class="copyright">
+												<p>Copyright 2014 all rights reserved, Designed by <a href="http://businessonmarketst.com">Business on Market St.</a></p>
+											</div>
+										</div>
+									</footer>
+								</body>
+								</html>
